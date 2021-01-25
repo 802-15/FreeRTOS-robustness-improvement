4ba32e4cb255f66a7a7a5aed979e57fb00d62930
Mario Senecic <mario.senecic@fer.hr>
2021-01-25 21:49:26 +0100

    set up RNG peripheral and an extra tracing pin
    Signed-off-by: Mario Senecic <mario.senecic@fer.hr>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "rng.h"
 #include "usart.h"
 #include "gpio.h"
 
